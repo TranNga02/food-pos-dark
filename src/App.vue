2791c5b676f21e333ed5b26902755c6fcb9f9cc6
Ngà Trần <tranngadn02@gmail.com>
2023-01-06 16:12:41 +0700

    push

--- src/App.vue
+++ src/App.vue
@@ -1,109 +1,15 @@
 <template>
   <div id="app">
     <HomePage></HomePage>
-    <!-- <MenuContent></MenuContent> -->
-    <!-- <div class="div">
-      
-      <div class="row my-2">
-        <div class="col-sm-2">
-        </div>
-        <div class="col-sm-2">
-          <DishCard v-bind:dishInfo=dishInfo></DishCard>
-        </div>
-        <div class="col-sm-2">
-          <CardDish v-bind:dishInfo=dishInfo></CardDish>
-        </div>
-        <div class="col-sm-2">
-          <SummaryCard></SummaryCard>
-        </div>
-        <div class="col-sm-4">
-          <MostOrdered v-bind:dishInfo=dishInfo></MostOrdered>
-        </div>
-        
-      </div>
-
-      <div class="row" v-for="(customer, index) in customers" v-bind:key="index">
-        <div class="col-sm-6"></div>
-        <div class="col-sm-6">
-          <OrderReportRow v-bind:customer="customer"></OrderReportRow>
-        </div>
-      </div>
-
-      <div class="row my-2">
-        <div class="col-sm-4"></div>
-        <div class="col-sm-4">
-          <ConfirmationItem v-bind:dishInfo=dishInfo></ConfirmationItem>
-        </div>
-        <div class="col-sm-4">
-          <OrderTab></OrderTab>
-        </div>
-      </div>
-
-      
-        
-      
-    </div> -->
   </div>
 </template>
 
 <script>
-// import DishCard from './components/DishCard/DishCard.vue';
-// import CardDish from './components/CardDish/CardDish.vue';
-// import SummaryCard from './components/SummaryCard/SummaryCard.vue';
-// import MostOrdered from './components/MostOrdered/MostOrdered.vue';
-// import OrderReportRow from './components/OrderReportRow/OrderReportRow.vue';
-// import ConfirmationItem from './components/ConfirmationItem/ConfirmationItem.vue';
-// import OrderTab from './components/OrderTab/OrderTab.vue';
-// import SideBar from './components/SideBar/SideBar.vue';
 import HomePage from './container/HomePage/HomePage.vue';
-// import MenuContent from './container/MenuContent/MenuContent.vue';
 export default {
   name: 'app',
-  data() {
-    return {
-      dishInfo: {
-        image: "@/assets/Image/noodles.png",
-        name: "Spicy seasoned seafood noodles",
-        price: 2.29,
-        quantity: 20,
-        ordered: 200
-      },
-      customers: [
-        {
-          name: "Eren Jaegar",
-          avatar: "@/assets/Avatar/Avatar1.png",
-          menu: "Spicy seasoned seafood noodles",
-          totalPayment: 125,
-          dishStatus: "completed"
-        },
-        {
-          name: "Reiner Braunn",
-          avatar: "@/assets/Avatar/Avatar2.png",
-          menu: "Salted Pasta with mushroom sauce",
-          totalPayment: 145,
-          dishStatus: "preparing"
-        },
-        {
-          name: "Levi Ackerman",
-          avatar: "@/assets/Avatar/Avatar3.png",
-          menu: "Beef dumpling in hot and sour soup",
-          totalPayment: 105,
-          dishStatus: "pending"
-        }
-      ]
-    }
-  },
   components: {
-    // DishCard,
-    // CardDish,
-    // SummaryCard,
-    // MostOrdered,
-    // OrderReportRow,
-    // ConfirmationItem,
-    // OrderTab,
-    // SideBar,
     HomePage,
-    // MenuContent
 }
 }
 </script>
