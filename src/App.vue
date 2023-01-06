ab36c19fe7317712f6cf489beeca4b79461b5298
Ngà Trần <tranngadn02@gmail.com>
2023-01-06 11:58:15 +0700

    Create SideBar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
-  <div id="app">
-    <div class="div">
+  <div id="app" class="container-fluid">
+    <div class="row">
+        <div class="col-sm-auto sticky-top p-0 d-flex align-items-stretch">
+          <SideBar></SideBar>
+          <!-- <BarSide></BarSide> -->
+        </div>
+        <div class="col-sm p-3 min-vh-100">
+            <!-- content -->
+        </div>
+    </div>
+    <!-- <div class="div">
+      
       <div class="row my-2">
+        <div class="col-sm-2">
+        </div>
         <div class="col-sm-2">
           <DishCard v-bind:dishInfo=dishInfo></DishCard>
         </div>
@@ -14,40 +26,42 @@
         <div class="col-sm-4">
           <MostOrdered v-bind:dishInfo=dishInfo></MostOrdered>
         </div>
-        <div class="col-sm-2">
-          <OrderStatus v-bind:dishStatus="'preparing'"></OrderStatus>
-        </div>
+        
       </div>
 
       <div class="row" v-for="(customer, index) in customers" v-bind:key="index">
+        <div class="col-sm-6"></div>
         <div class="col-sm-6">
           <OrderReportRow v-bind:customer="customer"></OrderReportRow>
         </div>
-        <div class="col-sm-6"></div>
       </div>
+
       <div class="row my-2">
+        <div class="col-sm-4"></div>
         <div class="col-sm-4">
           <ConfirmationItem v-bind:dishInfo=dishInfo></ConfirmationItem>
         </div>
         <div class="col-sm-4">
           <OrderTab></OrderTab>
         </div>
-        <div class="col-sm-4"></div>
-      </div>
+      </div> -->
 
+      
+        
+      
     </div>
-  </div>
+  <!-- </div> -->
 </template>
 
 <script>
-import DishCard from './components/DishCard/DishCard.vue';
-import CardDish from './components/CardDish/CardDish.vue';
-import SummaryCard from './components/SummaryCard/SummaryCard.vue';
-import MostOrdered from './components/MostOrdered/MostOrdered.vue';
-import OrderStatus from './components/OrderStatus/OrderStatus.vue';
-import OrderReportRow from './components/OrderReportRow/OrderReportRow.vue';
-import ConfirmationItem from './components/ConfirmationItem/ConfirmationItem.vue';
-import OrderTab from './components/OrderTab/OrderTab.vue';
+// import DishCard from './components/DishCard/DishCard.vue';
+// import CardDish from './components/CardDish/CardDish.vue';
+// import SummaryCard from './components/SummaryCard/SummaryCard.vue';
+// import MostOrdered from './components/MostOrdered/MostOrdered.vue';
+// import OrderReportRow from './components/OrderReportRow/OrderReportRow.vue';
+// import ConfirmationItem from './components/ConfirmationItem/ConfirmationItem.vue';
+// import OrderTab from './components/OrderTab/OrderTab.vue';
+import SideBar from './components/SideBar/SideBar.vue';
 export default {
   name: 'app',
   data() {
@@ -85,14 +99,14 @@ export default {
     }
   },
   components: {
-    DishCard,
-    CardDish,
-    SummaryCard,
-    MostOrdered,
-    OrderStatus,
-    OrderReportRow,
-    ConfirmationItem,
-    OrderTab
+    // DishCard,
+    // CardDish,
+    // SummaryCard,
+    // MostOrdered,
+    // OrderReportRow,
+    // ConfirmationItem,
+    // OrderTab,
+    SideBar
   }
 }
 </script>
@@ -102,8 +116,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 20px;
+  color: #2c3e50;                                                                                                                                                                              
 }
 @import url('./styles/body.scss');
 </style>
