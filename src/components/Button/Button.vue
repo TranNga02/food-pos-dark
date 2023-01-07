7a6098af94f8e4984c5b97a89329270590061b26
Ngà Trần <tranngadn02@gmail.com>
2023-01-07 21:38:15 +0700

    abc

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-btn :class="classNames" :type="type" :loading="loading" :disabled="disabled" @click="clickBtn">{{ label }}</v-btn>
+</template>
+
+<script lang="ts" src="./Button.ts"></script>
+
+<style scoped lang="scss" src="./style.scss"></style>
