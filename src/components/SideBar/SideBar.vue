40bb7918ce332feb19a2ba4d4d31238710a04a0c
Tran Nga <tranngadn02@gmail.com>
2023-01-17 21:02:37 +0700

    update FontIcon, SideBar 2

--- src/components/SideBar/SideBar.vue
+++ src/components/SideBar/SideBar.vue
@@ -1,22 +1,22 @@
 <template>
-  <v-navigation-drawer class="side-bar" :width="90">
+  <v-navigation-drawer class="side-bar" :width="90" color="#1f1d2b">
     <v-list-item class="item">
       <v-list-item-icon class="logo">
-        <FontIcon name="logo" :size="40"></FontIcon>
+        <FontIcon name="logo" :size="40" />
       </v-list-item-icon>
     </v-list-item>
     <v-list>
       <v-list-item v-for="item in items" class="item" :key="item.icon">
         <router-link :to="item.path" class="nav-link">
           <v-list-item-icon class="icon-button">
-            <FontIcon :name="item.icon"></FontIcon>
+            <FontIcon :name="item.icon" />
           </v-list-item-icon>
         </router-link>
       </v-list-item>
     </v-list>
     <v-list-item class="item">
       <v-list-item-icon class="icon-button">
-        <FontIcon name="log-out"></FontIcon>
+        <FontIcon name="log-out" />
       </v-list-item-icon>
     </v-list-item>
   </v-navigation-drawer>
