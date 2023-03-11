7bd6d71d9e740b1ed5efa66ea42f0adc7b0546da
Tran Nga <tranngadn02@gmail.com>
2023-03-11 10:17:47 +0700

    MenuContainer

--- src/containers/HomeContainer/elements/MenuContainer/MenuContainer.vue
+++ src/containers/HomeContainer/elements/MenuContainer/MenuContainer.vue
@@ -6,41 +6,37 @@
         <p class="date">{{ date }}</p>
       </div>
       <div>
-        <Input placeholder="Search for food, coffee, etc.." icon="search" solo />
+        <Input placeholder="Search for food, coffee, etc.." :onChange="changeSearch" icon="search" solo />
+      </div>
+    </div>
+    <div class="navbar">
+      <v-btn-toggle class="bt-group" v-model="menuValue" mandatory>
+        <v-btn
+          class="btn"
+          v-for="item in menuItems"
+          :key="item.value"
+          color="white"
+          text
+          height="25px"
+          min-width="fit-content"
+        >
+          {{ item.name }}
+        </v-btn>
+      </v-btn-toggle>
+    </div>
+    <div class="detail-header">
+      <div><p>Choose Dishes</p></div>
+      <div>
+        <DropDown :options="dropdownItems" :value="selectValue" label="Order Type" :onChange="changeSelect" />
       </div>
     </div>
 
-    <div class="content">
-      <div class="navbar">
-        <v-btn-toggle class="bt-group" v-model="menuValue" mandatory>
-          <v-btn
-            class="btn"
-            v-for="item in menuItems"
-            :key="item.value"
-            color="white"
-            text
-            height="25px"
-            min-width="fit-content"
-          >
-            {{ item.name }}
-          </v-btn>
-        </v-btn-toggle>
-      </div>
-      <div class="detail">
-        <div class="detail-header">
-          <div><p>Choose Dishes</p></div>
-          <div>
-            <DropDown :options="dropdownItems" :value="selectValue" label="Order Type" :onChange="changeSelect" />
-          </div>
-        </div>
-        <div class="detail-content">
-          <v-row>
-            <v-col md="4" class="col-sm-4" v-for="(dish, index) in dishInfo" :key="index">
-              <DishCard width="250px" :dishInfo="dish" />
-            </v-col>
-          </v-row>
-        </div>
-      </div>
+    <div class="detail">
+      <v-row>
+        <v-col md="4" class="col-sm-4" v-for="dish in dishInfo" :key="dish.id">
+          <DishCard width="250px" :dishInfo="dish" :onChangeDish="changeDishOrder" :className="isActive(dish.id)" />
+        </v-col>
+      </v-row>
     </div>
   </div>
 </template>
