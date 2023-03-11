02e090580fa7ac4ae31d279b8c84bacc55996482
Tran Nga <tranngadn02@gmail.com>
2023-03-11 10:17:18 +0700

    DishCard

--- src/containers/HomeContainer/elements/DishCard/DishCard.ts
+++ src/containers/HomeContainer/elements/DishCard/DishCard.ts
@@ -3,4 +3,21 @@ import { Vue, Prop, Component } from 'vue-property-decorator';
 @Component({})
 export default class DishCard extends Vue {
   @Prop({ required: true }) readonly dishInfo: Dish.DishInfo;
+  @Prop({}) readonly className: string;
+  @Prop({
+    default: () => {
+      //
+    },
+  })
+  readonly onChangeDish: (dish: number) => void;
+
+  get classNames(): string {
+    return `dish-card ${this.className || ''}`;
+  }
+
+  onClick(): void {
+    if (this.onChangeDish) {
+      this.onChangeDish(this.dishInfo.id);
+    }
+  }
 }
