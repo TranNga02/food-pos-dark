1ae2aed5fcee9bf4c6368be32646e330080f1a02
Tran Nga <tranngadn02@gmail.com>
2023-03-18 13:02:38 +0700

    update OrderContainer

--- src/containers/HomeContainer/elements/OrderContainer/OrderContainer.ts
+++ src/containers/HomeContainer/elements/OrderContainer/OrderContainer.ts
@@ -8,12 +8,14 @@ import ConfirmDishCard from '../ConfirmDishCard';
 })
 export default class OrderContainer extends Vue {
   @Prop({ default: '' }) readonly orderId: string;
+  @Prop({ required: true }) readonly selectValue: string;
   @Prop({ required: true }) readonly tabOptions: App.Option[];
   @Prop({ required: true }) readonly dishesOrder: Dish.DishOrder[];
+  @Prop({ required: true }) readonly changeSelect: (value: string) => void;
   @Prop({ required: true }) readonly changeNoteQuantity: (dishId: number, input: Dish.Input) => void;
   @Prop({ required: true }) readonly onDelete: (dishId: number) => void;
+  @Prop({ required: true }) readonly showPopUp: () => void;
 
-  selectValue = '0';
   subTotal = 0;
   discount = 0;
 
@@ -24,9 +26,5 @@ export default class OrderContainer extends Vue {
     }
     this.subTotal -= this.discount;
     return Number(this.subTotal.toFixed(2));
-  }
-
-  changeSelect(value: string): void {
-    this.selectValue = value;
   }
 }
