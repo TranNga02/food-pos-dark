3590ae2d31a640c66cdc15897ade37898c985a00
Tran Nga <tranngadn02@gmail.com>
2023-03-18 13:14:32 +0700

    update PaymentContainer

---
+++ src/containers/HomeContainer/elements/PaymentContainer/PaymentContainer.ts
@@ -0,0 +1,18 @@
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import Button from '@/components/Button';
+import Input from '@/components/Input';
+import DropDown from '@/components/DropDown';
+import PaymentMethod from '../PaymentMethod';
+
+@Component({
+  components: { Button, PaymentMethod, Input, DropDown },
+})
+export default class PaymentContainer extends Vue {
+  @Prop({ default: 0 }) readonly numberPayment: number;
+  @Prop({ required: true }) readonly selectValue: string;
+  @Prop({ required: true }) readonly dropdownItems: App.Option[];
+  @Prop({ required: true }) readonly paymentInfo: Payment.CreditCardInfo;
+  @Prop({ required: true }) readonly paymentMethods: Payment.PaymentMethod[];
+  @Prop({ required: true }) readonly showPopUp: () => void;
+  @Prop({ required: true }) readonly changeSelect: (value: string) => void;
+}
